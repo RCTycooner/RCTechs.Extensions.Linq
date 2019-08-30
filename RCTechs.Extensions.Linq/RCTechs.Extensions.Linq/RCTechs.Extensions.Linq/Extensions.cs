94227053602845deb43e653c4acaf19f846a0da5
Tom Wuyts <tom.wuyts@calidos.be>
2019-08-30 13:15:52 +0200

    created nugetpackage

---
+++ RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RCTechs.Extensions.Linq
+{
+    public static class Extensions
+    {
+        public static TResult MaxOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            return source
+                .Select(x => selector(x))
+                .DefaultIfEmpty()
+                .Max();
+        }
+    }
+}
