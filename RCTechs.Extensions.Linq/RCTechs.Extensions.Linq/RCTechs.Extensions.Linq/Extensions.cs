620ac188d7d92241a1d842f92ca07f2593c7ecd3
Tom Wuyts <tom.wuyts@calidos.be>
2019-08-30 13:46:34 +0200

    fixed naming typo

--- RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/Extensions.cs
+++ RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/Extensions.cs
@@ -31,7 +31,7 @@ namespace RCTechs.Extensions.Linq
         /// <param name="source"></param>
         /// <param name="selector"></param>
         /// <returns></returns>
-        public static TResult MInOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        public static TResult MinOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
             return source
                 .Select(x => selector(x))
