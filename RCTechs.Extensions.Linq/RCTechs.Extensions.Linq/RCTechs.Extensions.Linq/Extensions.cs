e22fba97ac3cf58778088166c7358cb7d82042a5
Tom Wuyts <tom.wuyts@calidos.be>
2019-08-30 13:46:11 +0200

    added MinOrDefault

--- RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/Extensions.cs
+++ RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/RCTechs.Extensions.Linq/Extensions.cs
@@ -6,12 +6,37 @@ namespace RCTechs.Extensions.Linq
 {
     public static class Extensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
         public static TResult MaxOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
             return source
                 .Select(x => selector(x))
                 .DefaultIfEmpty()
                 .Max();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static TResult MInOrDefault<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            return source
+                .Select(x => selector(x))
+                .DefaultIfEmpty()
+                .Min();
         }
     }
 }
